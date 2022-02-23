990f362d96acce5d3585f0b473866ef51ece259e
Deg42 <jgonzalez@eci.safanet.es>
2022-02-23 08:51:49 +0100

    working

--- src/main/java/es/fpdual/consumeAemet/ConsumeAemetApplication.java
+++ src/main/java/es/fpdual/consumeAemet/ConsumeAemetApplication.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ConsumeAemetApplication {
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ConsumeAemetApplication.class, args);
 	}
+
+
 
 }
